d5433750ccf9813843fcca7d79d2b5bc101550b6
Rustam Singh <russingh@cisco.com>
2017-11-08 11:35:13 +0530

    minor url tweak

--- wsgi.py
+++ wsgi.py
@@ -10,7 +10,7 @@ def hello():
     ocfirst_port = os.environ['OCFIRST_SERVICE_PORT']
     url = 'http://%s:%s/' % (ocfirst_host, ocfirst_port)
     print 'dispatching request to %s' % url
-    r = requests.get('http://ocfirst/')
+    r = requests.get(url)
     print r.text
     return r.text
 
