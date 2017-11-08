29b5c9370dd237a27b50826be61c34e07282b84e
Rustam Singh <russingh@cisco.com>
2017-11-08 10:43:32 +0530

    added env variable pulling

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,15 @@
 from flask import Flask
 import requests
+import os
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    print "dispatching request"
+    ocfirst_host = os.environ['OCFIRST_SERVICE_HOST']
+    ocfirst_port = os.environ['OCFIRST_SERVICE_PORT']
+    url = 'https://%s:%s/' % (ocfirst_host, ocfirst_port)
+    print 'dispatching request to %s' % url
     r = requests.get('http://ocfirst/')
     print r.text
     return r.text
